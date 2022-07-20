aea68293d137fe250563f23adb7c43bb342b3c04
psj98 <park98sj1@hanmail.net>
2022-07-21 00:52:47 +0900

    C++ 함수

--- C++/5. 함수/1065.cpp
+++ C++/5. 함수/1065.cpp
@@ -2,13 +2,33 @@
 
 using namespace std;
 
-int main(){
-    char n[6];
-    scanf("%s", n);
-    string s=n;
+int check(int n){
+    int cnt=0;
+    for(int i=1; i<=n; i++){
+        if(i<=99) cnt++;
+        else{
+            int a=i%10;
+            int b=(i/10)%10;
+            int c=i/100;
+            
+            int l=a-b;
+            int m=b-c;
 
+            if(l==m)
+                cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    int ans=check(n);
     
-    
+    printf("%d", ans);
 
     return 0;
 }
